caece4137d8a4539f4aee7190399df252d0f03e7
Paul Stey <paulstey@gmail.com>
2023-05-09 15:37:26 -0400

    build: move some things around and general housekeeping

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
 use rand::{self, Rng};
 use time::Instant;
-
-// This function computes the Collatz sequence of numbers. This is sometimes also known as 
-// the "hailstone sequence" 
-fn collatz(mut n: u32) -> Vec<u32> {
-
-    let mut collatz_sequence = vec![n]; // initialize the vector with the starting number
-    
-    while n != 1 {
-        match n % 2 {
-            0 => n /= 2,
-            1 => n = n * 3 + 1,
-            _ => unreachable!(),
-        }
-        collatz_sequence.push(n);       // add the current number to the vector
-    }
-
-    collatz_sequence
-}
-
+use collatz::collatz;
 
 
 fn main() {
