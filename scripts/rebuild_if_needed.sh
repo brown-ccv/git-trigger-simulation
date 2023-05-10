6f15e00e3a3b4cf755759442ccddfba9066137fb
Paul Stey <paulstey@gmail.com>
2023-05-10 13:52:41 -0400

    build: simulation 7

--- scripts/rebuild_if_needed.sh
+++ scripts/rebuild_if_needed.sh
@@ -6,12 +6,14 @@ git pull origin devel
 NUM_COMMITS_TO_CHECK=20
 
 # Get the latest commit hash, and check if we've already built from that commit 
-# LASTHASH will be the empty string if we haven't built from this commit 
-LAST_BUILD_HASH=`git --no-pager log --pretty=oneline -n $NUM_COMMITS_TO_CHECK | grep build: | head -n 1 | cut -d ' ' -f 1 | xargs -I {} grep {} ./logs/previous_build_hashes.txt`
+# LAST_BUILD_COMMIT will be the empty string if we haven't built from this commit 
+LAST_BUILD_COMMIT=`git --no-pager log --pretty=oneline -n $NUM_COMMITS_TO_CHECK | grep build: | head -n 1 | cut -d ' ' -f 1`
+grep -q $LAST_BUILD_COMMIT ./logs/previous_build_hashes.txt
+GREP_EXIT_STATUS=$? # get grep exit status, which will be `0` if it found LAST_BUILD_COMMIT 
 
-echo $LAST_BUILD_HASH 
+echo "The last build hash is: $LAST_BUILD_COMMIT" 
 
-if [ -z "$LAST_BUILD_HASH" ]; then
+if [ $GREP_EXIT_STATUS -ne 0 ]; then
     echo "We need to rebuild!!"
     sbatch submit.sh
     git --no-pager log --pretty=oneline -n $NUM_COMMITS_TO_CHECK | grep build: | head -n 1 | cut -d ' ' -f 1 >> ./logs/previous_build_hashes.txt
