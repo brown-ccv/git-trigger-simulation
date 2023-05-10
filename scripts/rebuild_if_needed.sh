194b83110057e262f31311946be5bef770e300b4
Paul Stey <paulstey@gmail.com>
2023-05-10 11:48:38 -0400

    build: fix the logic in rebuild_if_needed.sh

--- scripts/rebuild_if_needed.sh
+++ scripts/rebuild_if_needed.sh
@@ -5,12 +5,11 @@ git pull origin devel
 
 # Get the latest commit hash, and check if we've already built from that commit 
 # LASTHASH will be the empty string if we haven't built from this commit 
-LASTHASH=`git --no-pager log --pretty=oneline -n1 | cut -d' ' -f 1 | xargs -I {} grep {} ./logs/previous_build_hashes.txt`
-BUILD=`git --no-pager log --pretty=%s -n1 | grep '^build:'`
+LAST_BUILD_HASH=`git --no-pager log --pretty=oneline -n 10 | grep build: | head -n 1 | cut -d ' ' -f 1 | xargs -I {} grep {} ./logs/previous_build_hashes.txt`
 
-echo $LASTHASH && echo $BUILD
+echo $LAST_BUILD_HASH 
 
-if [ -z "$LASTHASH" ] && [ ! -z "$BUILD" ]; then
+if [ -z "$LAST_BUILD_HASH" ]; then
     echo "We need to rebuild!!"
     sbatch submit.sh
     git --no-pager log --pretty=oneline -n1 | cut -d' ' -f 1 >> ./logs/previous_build_hashes.txt
